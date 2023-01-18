bdb4eb0f1295bd45208f7ebd668263b5f6d8b65d
serendipitousa <serendipitousa@gmail.com>
2023-01-18 20:46:47 +0200

    Initial commit

---
+++ src/com/ua/robot/Main.java
@@ -0,0 +1,11 @@
+package com.ua.robot;
+public class Main {
+    public static void main(String[] args) {
+        int number1 = 6;
+        int number2 = 3;
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 * number2);
+    }
+}
